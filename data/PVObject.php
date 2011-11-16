83669b21d64736a0314ff8e52bf7d902abc199af
ProdigyView <devindixon@BlackMage.local>
2011-11-16 13:39:54 -0500

    Added adapters, filters and observers. Also added abiltiy to add annoynmous function.

--- data/PVObject.php
+++ data/PVObject.php
@@ -30,43 +30,122 @@
 class PVObject extends PVPatterns {
 
 	protected $_collection = null;
+	
+	protected $_methods = array();
 
 	public function __set($index, $value) {
+		
+		if ($this->_hasAdapter(get_class(), __FUNCTION__))
+			return $this->_callAdapter(get_class(), __FUNCTION__, $index, $value);
+		
+		$filtered = $this->_applyFilter(get_class(), __FUNCTION__, array('index' => $index, 'value' => $value), array('event' => 'args'));
+		$index = $filtered['index'];
+		$value = $filtered['value'];
+		
 		if ($this -> _collection == null) {
 			$this -> _collection = new PVCollection();
 		}
 		$this -> _collection -> addWithName($index, $value);
+		$this->_notify(get_class() . '::' . __FUNCTION__, $index, $value);
 	}
 
 	public function __get($index) {
+		
+		if ($this->_hasAdapter(get_class(), __FUNCTION__))
+			return $this->_callAdapter(get_class(), __FUNCTION__, $index);
+		
+		$index = $this->_applyFilter(get_class(), __FUNCTION__, $index, array('event' => 'args'));
+		
 		if ($this -> _collection == null) {
 			$this -> _collection = new PVCollection();
 		}
-		return $this -> _collection -> get($index);
+		
+		$value = $this -> _collection -> get($index);
+		
+		$this->_notify(get_class() . '::' . __FUNCTION__, $value, $index);
+		$value = $this->_applyFilter(get_class(), __FUNCTION__, $value, array('event' => 'return'));
+		
+		return $value;
+	}
+	
+	function __call($method,$args) {
+  			
+		if ($this->_hasAdapter(get_class(), __FUNCTION__))
+			return $this->_callAdapter(get_class(), __FUNCTION__, $method, $args);
+		
+		$filtered = $this->_applyFilter(get_class(), __FUNCTION__, array('method' => $method, 'args' => $args), array('event' => 'args'));
+		$method = $filtered['method'];
+		$args = $filtered['args'];
+		
+  		if(isset($this->_methods[$method]))
+  			$value = call_user_func_array($this->_methods[$method] , $args);
+		else 
+			$value = null;
+		
+		$this->_notify(get_class() . '::' . __FUNCTION__, $value, $method, $args);
+		$value = $this->_applyFilter(get_class(), __FUNCTION__, $value, array('event' => 'return'));
+		
+		return $value;
 	}
 
-	protected function addToCollection($data) {
+	protected function _addToCollection($data) {
+		
+		if ($this->_hasAdapter(get_class(), __FUNCTION__))
+			return $this->_callAdapter(get_class(), __FUNCTION__, $data);
+		
+		$data = $this->_applyFilter(get_class(), __FUNCTION__, $data, array('event' => 'args'));
+		
 		if ($this -> _collection == null) {
 			$this -> _collection = new PVCollection();
 		}
 		$this -> _collection -> add($data);
+		$this->_notify(get_class() . '::' . __FUNCTION__, $data);
 	}//end
 
-	protected function addToCollectionWithName($name, $data) {
+	protected function _addToCollectionWithName($name, $data) {
+		
+		if ($this->_hasAdapter(get_class(), __FUNCTION__))
+			return $this->_callAdapter(get_class(), __FUNCTION__, $name, $data);
+		
+		$filtered = $this->_applyFilter(get_class(), __FUNCTION__, array('name' => $name, 'data' => $data), array('event' => 'args'));
+		$name = $filtered['name'];
+		$data = $filtered['data'];
+		
 		if ($this -> _collection == null) {
 			$this -> _collection = new PVCollection();
 		}
 		$this -> _collection -> addWithName($name, $data);
+		$this->_notify(get_class() . '::' . __FUNCTION__, $name, $data);
 	}//end
 
 	public function getIterator() {
+		
+		if ($this->_hasAdapter(get_class(), __FUNCTION__))
+			return $this->_callAdapter(get_class(), __FUNCTION__);
+		
 		if ($this -> _collection == null) {
 			$this -> _collection = new PVCollection();
 		}
 		return $this -> _collection -> getIterator();
 	}
+	
+	public function addMethod($method, $closure) {
+		
+		if ($this->_hasAdapter(get_class(), __FUNCTION__))
+			return $this->_callAdapter(get_class(), __FUNCTION__, $method, $closure);
+		
+		$filtered = $this->_applyFilter(get_class(), __FUNCTION__, array('method' => $method, 'closure' => $closure), array('event' => 'args'));
+		$method = $filtered['method'];
+		$closure = $filtered['closure'];
+		
+		$this->_methods[$method]=$closure;
+		$this->_notify(get_class() . '::' . __FUNCTION__, $method, $closure);
+	}
 
 	protected function getSqlSearchDefaults() {
+		
+		if ($this->_hasAdapter(get_class(), __FUNCTION__))
+			return $this->_callAdapter(get_class(), __FUNCTION__);
 			
 		$defaults = array(
 			'custom_where' => '', 
@@ -85,6 +164,8 @@ class PVObject extends PVPatterns {
 			'prefix_args' => ''
 		);
 
+		$defaults = $this->_applyFilter(get_class(), __FUNCTION__, $defaults, array('event' => 'return'));
+		
 		return $defaults;
 	}
 
