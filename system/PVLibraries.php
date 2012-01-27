d16abe631d40ee13a889f0860877280bf15d0598
ProdigyView <devindixon@BlackMage.local>
2012-01-26 20:31:42 -0500

    Made the init method static

--- system/PVLibraries.php
+++ system/PVLibraries.php
@@ -37,7 +37,7 @@ class PVLibraries extends PVStaticObject {
 	private static $open_javascript;
 	private static $libraries;
 
-	function init($config = array()) {
+	public static function init($config = array()) {
 
 		if (self::_hasAdapter(get_class(), __FUNCTION__))
 			return self::_callAdapter(get_class(), __FUNCTION__, $config);
