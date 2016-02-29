d9a4fc59ea1ee6d08f55dc527b7f6f66ef011ee1
ProdigyView <webmaster@prodigyview.com>
2016-02-29 01:00:29 -0500

    Fixing bug if HTTP accept not present

--- network/PVRequest.php
+++ network/PVRequest.php
@@ -23,7 +23,7 @@ class PVRequest extends PVStaticInstance {
 		
 		$defaults = array(
 			'process_request' => true,
-			'http_accept' =>  (strpos($_SERVER['HTTP_ACCEPT'], 'json')) ? 'json' : 'xml',
+			'http_accept' =>  (isset($_SERVER['HTTP_ACCEPT']) && strpos($_SERVER['HTTP_ACCEPT'], 'json')) ? 'json' : 'xml',
 			'request_method' => 'REQUEST_METHOD',
 			'mobile_devices' => "/(nokia|iphone|android|motorola|^mot-|softbank|foma|docomo|kddi|up.browser|up.link|htc|dopod|blazer|netfront|helio|hosin|huawei|novarra|CoolPad|webos|techfaith|palmsource|blackberry|alcatel|amoi|ktouch|nexian|samsung|^sam-|s[cg]h|^lge|ericsson|philips|sagem|wellcom|bunjalloo|maui|symbian|smartphone|midp|wap|phone|windows ce|iemobile|^spice|^bird|^zte-|longcos|pantech|gionee|^sie-|portalmmm|jigs browser|hiptop|^ucweb|^benq|haier|^lct|operas*mobi|opera*mini|320x320|240x320|176x220)/i"		
 			);
