b5e5893b8bea010b520db7e273c147e607bb9cb9
ProdigyView <webmaster@prodigyview.com>
2016-07-14 18:56:24 -0400

    Fixing -i

--- media/PVAudio.php
+++ media/PVAudio.php
@@ -361,7 +361,7 @@ class PVAudio extends PVStaticObject {
 		$input_options = self::setEncodingOptions($options, 'input_');
 		$output_options = self::setEncodingOptions($options, 'output_');
 
-		exec("$converter -i $input_options $current_file_location -y $output_options $new_file_location ");
+		exec("$converter $input_options -i $current_file_location -y $output_options $new_file_location ");
 		self::_notify(get_class() . '::' . __FUNCTION__, $current_file_location, $new_file_location, $options, $input_options, $output_options);
 	}//end convertAudioFile
 
