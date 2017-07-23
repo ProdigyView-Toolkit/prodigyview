4a51cad2bb93a94e60234a4759a16c8567a6e163
ProdigyView <webmaster@prodigyview.com>
2017-07-23 11:41:03 -0400

    Updating hast string algoritgtm

--- system/PVSecurity.php
+++ system/PVSecurity.php
@@ -1927,7 +1927,8 @@ class PVSecurity extends PVStaticObject {
 		$string = $filtered['string'];
 		$salt = $filtered['salt'];
 		
-		$hashed_string = crypt( $string, $salt ?: self::$_salt );
+		//$hashed_string = crypt( $string, $salt ?: self::$_salt );
+		$hashed_string = password_hash($string, PASSWORD_BCRYPT, array('cost' => 10, 'salt' => $salt ?: self::$_salt));
 		
 		self::_notify(get_class() . '::' . __FUNCTION__, $hashed_string, $string, $salt);
 		$hashed_string = self::_applyFilter(get_class(), __FUNCTION__, $hashed_string, array('event' => 'return'));
